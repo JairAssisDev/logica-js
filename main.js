b94a997f3ac362c5f8cfb4f5bb161b918328a78b
JairAssisDev <jairv141@gmail.com>
2023-04-06 15:05:01 -0300

    atlz

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function mostra(mostra){
-    document.write(mostra);
-    document.write("<br><br>");
+function mostra(mostra,x){
+    if (x==0){
+        document.write(mostra);
+    }else{
+        document.write(mostra);
+        document.write("<br><br>");
+    }
+
 }
 var ano = 2023;
 var idadeFlavio = ano - 1978;
@@ -10,8 +15,8 @@ var media = (idadeFlavio+ idadeJoaquin+idadeBarney)/3;
 var nome1="Flavio";
 var nome2="Joaquin";
 var nome3="Barney";
-mostra("Flávio nasceu em " + idadeFlavio );
-mostra("Joaquim nasceu em " + idadeJoaquin );
-mostra("Barney nasceu em " + idadeBarney );
-mostra("a media das idades é : "+Math.round(media));
-mostra("A idade de "+ nome1+ " é "+ idadeFlavio); 
+mostra("Flávio nasceu em " + idadeFlavio ,1);
+mostra("Joaquim nasceu em " + idadeJoaquin ,1);
+mostra("Barney nasceu em " + idadeBarney ,1);
+mostra("a media das idades é : "+Math.round(media),1);
+mostra("A idade de "+ nome1+ " é "+ idadeFlavio,1); 
