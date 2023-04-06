3e112e460ee82846c5613b52cc886bc5fc1c171f
JairAssisDev <jairv141@gmail.com>
2023-04-06 13:55:45 -0300

    atz

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-var ano = 2023
+function pulaLinda(){
+    document.write("<br><br>");
+}
+function mostra(mostra){
+    document.write(mostra);
+}
+var ano = 2023;
 var idadeFlavio = ano - 1978;
 var idadeJoaquin = ano - 1997;
 var idadeBarney = ano - 1976;
-    document.write("Flávio nasceu em " + idadeFlavio );
-    document.write("<br>");
-    document.write("Joaquim nasceu em " + idadeJoaquin );
-    document.write("<br>");
-    document.write("Barney nasceu em " + idadeBarney );
-    document.write("<br>");
-    var media = (idadeFlavio+ idadeJoaquin+idadeBarney)/3;
-    document.write("a media das idades é : "+Math.round(media));
-    document.write("<br>");
-    var nome1="Flavio";
-    var nome2="Joaquin";
-    var nome3="Barney";
-
-    document.write("A idade de "+ nome1+ " é "+ idadeFlavio); 
+var media = (idadeFlavio+ idadeJoaquin+idadeBarney)/3;
+var nome1="Flavio";
+var nome2="Joaquin";
+var nome3="Barney";
+mostra("Flávio nasceu em " + idadeFlavio );
+pulaLinda();
+mostra("Joaquim nasceu em " + idadeJoaquin );
+pulaLinda();
+mostra("Barney nasceu em " + idadeBarney );
+pulaLinda();
+mostra("a media das idades é : "+Math.round(media));
+pulaLinda();
+mostra("A idade de "+ nome1+ " é "+ idadeFlavio); 
