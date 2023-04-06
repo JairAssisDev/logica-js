bea89b613cf11a6eba1e75caa09b6fbb6e1777ab
JairAssisDev <jairv141@gmail.com>
2023-04-06 14:13:45 -0300

    atlz

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-function pulaLinda(){
-    document.write("<br><br>");
-}
 function mostra(mostra){
     document.write(mostra);
+    document.write("<br><br>");
 }
 var ano = 2023;
 var idadeFlavio = ano - 1978;
@@ -13,11 +11,7 @@ var nome1="Flavio";
 var nome2="Joaquin";
 var nome3="Barney";
 mostra("Flávio nasceu em " + idadeFlavio );
-pulaLinda();
 mostra("Joaquim nasceu em " + idadeJoaquin );
-pulaLinda();
 mostra("Barney nasceu em " + idadeBarney );
-pulaLinda();
 mostra("a media das idades é : "+Math.round(media));
-pulaLinda();
 mostra("A idade de "+ nome1+ " é "+ idadeFlavio); 
