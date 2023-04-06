46cd8bf6d548819174525daf3da9e489699b390c
JairAssisDev <jairv141@gmail.com>
2023-04-06 12:26:33 -0300

    atz

---
+++ main.js
@@ -0,0 +1,18 @@
+var ano = 2023
+var idadeFlavio = ano - 1978;
+var idadeJoaquin = ano - 1997;
+var idadeBarney = ano - 1976;
+    document.write("Flávio nasceu em " + idadeFlavio );
+    document.write("<br>");
+    document.write("Joaquim nasceu em " + idadeJoaquin );
+    document.write("<br>");
+    document.write("Barney nasceu em " + idadeBarney );
+    document.write("<br>");
+    var media = (idadeFlavio+ idadeJoaquin+idadeBarney)/3;
+    document.write("a media das idades é : "+Math.round(media));
+    document.write("<br>");
+    var nome1="Flavio";
+    var nome2="Joaquin";
+    var nome3="Barney";
+
+    document.write("A idade de "+ nome1+ " é "+ idadeFlavio); 
