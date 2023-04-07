4595f9b62b40c835b9de707ed7050b86d9c9e077
JairAssisDev <jairv141@gmail.com>
2023-04-07 11:14:27 -0300

    atlz

--- main2.js
+++ main2.js
@@ -7,12 +7,30 @@ function mostra(frase){
 }
 function calculaImc(altura, peso) {
 
-    var imc = peso / (altura * altura);
-    mostra("O imc calculado é " + imc);
+    return peso / (altura * altura);
 }
-var imcFlavio = calculaImc(1.72, 73);
-var imcPaulo = calculaImc(1.72, 68);
 
-var totalImc = imcFlavio+imcPaulo;
+var nome = prompt("Informe o seu nome");
+var alturaInformada = prompt(nome + ", informe sua altura");
+var pesoInformado = prompt(nome + ", informe seu peso");
 
-document.write("A soma dos Imc's é "+ totalImc);
+var imc = calculaImc(alturaInformada, pesoInformado);
+
+mostra(nome + ", o seu IMC é " + imc);
+
+if(imc < 18.5) {
+
+    mostra("Você está abaixo do recomendado");
+
+}
+
+if(imc > 35) {
+
+    mostra("Você está acima do recomendado");
+}
+
+if(imc >= 18.5 && imc <= 35) {
+
+    mostra("Seu IMC está excelente!");
+
+}
